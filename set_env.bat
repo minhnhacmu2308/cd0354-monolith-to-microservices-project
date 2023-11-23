84b46f43163eeacca05fac8a054472f39d30fec8
Your Name <you@example.com>
2023-11-23 15:41:08 +0700

    first commit

---
+++ set_env.bat
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+set POSTGRES_USERNAME=postgres
+set POSTGRES_PASSWORD=12345678
+set POSTGRES_HOST=crd1.c25lerxwtvzq.us-east-1.rds.amazonaws.com
+set POSTGRES_DB=postgres
+set AWS_BUCKET=test-359967743302-dev
+set AWS_REGION=us-east-1
+set AWS_PROFILE=default
+set JWT_SECRET=testing
+set URL=http://localhost:8100
